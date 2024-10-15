d74f63478eb7c1ef4773279c57347adc10afcd81
SV <SV@PM19-PC01>
2024-10-15 15:11:19 +0700

    Add project files.

---
+++ Models/ViewModel/ProductSearchVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _23DH113217_MyStore.Models.ViewModel
+{
+    public class ProductSearchVM
+    {
+        public string SearchTerm { get; set; }
+        public List<Product> Products { get; set; }
+
+        public string SortOrder { get; set; }
+
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
